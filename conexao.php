96d9c5d196dc8067e7a777817d60101b04fa63aa
JIsaquewillers <joaoisaquewillers@gmail.com>
2026-07-10 21:20:31 -0300

    .

--- conexao.php
+++ conexao.php
@@ -2,7 +2,7 @@
 $servidor = "localhost";
     $usuario = "root";
     $senha = "";
-    $banco = "empresa67";
+    $banco = "empresa26";
 
     $conexao = mysqli_connect($servidor, $usuario, $senha, $banco);
     ?>
